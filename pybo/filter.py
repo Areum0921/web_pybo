5bdfd67ed3d21fe1bc336b772ebf50c71730c680
Areum0921 <a90907@gmail.com>
2021-04-29 18:12:05 +0900

    내 질문글 모아보기 ver 1.2

--- pybo/filter.py
+++ pybo/filter.py
@@ -1,7 +1,7 @@
 import locale
 locale.setlocale(locale.LC_ALL,'')
 
-def format_datetime(value, fmt='%Y년 %m월 %d일 %H:%M'):
+def format_datetime(value, fmt='%Y %m %d %H:%M'):
     return value.strftime(fmt)
 
 def format_ip(value):
